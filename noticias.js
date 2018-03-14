5c0948f514e189c8db230b47be6820f4ba492672
LeandroBelge <leandrobelge@gmail.com>
2018-03-14 14:42:33 -0300

    Respondendo requisições http

--- noticias.js
+++ noticias.js
@@ -1,1 +1,9 @@
-console.log('Criando um site de notícias com NodeJS');
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+
+    res.end("<html><body>Portal de notícias</body></html>");
+
+});
+
+server.listen(3000);
