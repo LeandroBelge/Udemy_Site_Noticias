32f017ee2a047da349f521104119e4cc7ec53995
LeandroBelge <leandrobelge@gmail.com>
2018-03-15 09:28:35 -0300

    Organizando o projeto e recuperando as rotas de módulos com retorno de funções

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 var app = require('./config/server');
 
-app.get('/', function(req, res){
-    res.render("home/index");
-});
-
-app.get('/formulario_inclusao_noticia', function(re,res){
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(re,res){
-    res.render("noticias/noticias");
-});
-
+var rotaHome = require('./app/routes/home')(app);
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaFormularioInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
 app.listen(3000, function(){
     console.log("Servidor rodando");
 })
