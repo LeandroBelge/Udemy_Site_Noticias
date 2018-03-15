7399836164414ebc4ad6132de9db2cdfef720841
LeandroBelge <leandrobelge@gmail.com>
2018-03-15 14:09:44 -0300

    Criando uma conexão e recuperando dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,17 @@
 module.exports = function(app){
     app.get('/noticias', function(re,res){
-        res.render("noticias/noticias");
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : 'admin',
+            database : 'portal_noticias'
+        });
+        var sql = 'select * from noticias';
+        connection.query(sql, function(error, result){
+            res.send(result);
+        });
+    
     });
 };
