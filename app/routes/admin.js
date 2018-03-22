7f7ac1817190664176acbeb5ae67d04e7e90b7a7
LeandroBelge <leandrobelge@gmail.com>
2018-03-22 08:35:55 -0300

    Inserindo body-parser middleware na aplicação

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,4 +2,9 @@ module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(re,res){
         res.render("admin/form_add_noticia");
     });
+
+    app.post('/noticias/salvar', function(re,res){
+        var noticias = re.body;
+        res.send(noticias);
+    });
 }
--- config/server.js
+++ config/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var consign = require('consign');//Gerenciador de routes
+var bodyParser = require('body-parser');//middleware
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));//Trata os métodos de requisição e resposta dos formulários
 
 consign()
     .include('app/routes')
