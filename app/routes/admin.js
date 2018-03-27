f4cc5e2a5279337a600d4cb70e41d4cfafd525de
LeandroBelge <leandrobelge@gmail.com>
2018-03-27 08:57:06 -0300

    Instalando e iniciando com o Express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,22 @@ module.exports = function(app){
 
     app.post('/noticias/salvar', function(re,res){
         var noticia = re.body;
+
+        //Validações
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty().isDate({format: 'YYY-MM-DD'});
+        req.assert('noticia', 'Noticia é obrigatória').notEmpty();
+
+        var erros = req.validationErros();
+
+        if(erros){
+            res.render('admin/form_add_noticia');
+            return;
+        }
+
         var connection = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(connection);//Instancia a classe noticiasModel
     
--- config/server.js
+++ config/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var consign = require('consign');//Gerenciador de routes
 var bodyParser = require('body-parser');//middleware
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));//Trata os métodos de requisição e resposta dos formulários
-
+app.use(expressValidator());
 consign()
     .include('app/routes')
     .then('config/dbConnection.js')
