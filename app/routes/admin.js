1dc01262645a966ceb9f535d287f5fe4c78acde9
LeandroBelge <leandrobelge@gmail.com>
2018-04-02 18:22:24 -0300

    Mantendo dados do cadastro na tela

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(re,res){
-        res.render("admin/form_add_noticia");
+        res.render("admin/form_add_noticia", {validacao : {}, noticia : {}});
     });
 
     app.post('/noticias/salvar', function(req,res){
@@ -17,7 +17,7 @@ module.exports = function(app){
         var erros = req.validationErrors();
 
         if(erros){
-            res.render('admin/form_add_noticia', {validacao : erros});
+            res.render('admin/form_add_noticia', {validacao : erros, noticia : noticia});
             return;
         }
 
